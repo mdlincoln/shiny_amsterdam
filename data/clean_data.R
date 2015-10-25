6b4db411d41c66e42250af669e87f70adcf63396
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2015-10-25 11:51:33 -0400

    don't need any of this precomputed data

--- data/clean_data.R
+++ data/clean_data.R
@@ -29,48 +29,6 @@ multi_row_data <- orig_landscape_data %>%
 # places specified in name_coordinates
 location_data <- name_coordinates %>% inner_join(multi_row_data, by="place") %>% select(-description) %>% filter(type != "")
 
-# Categorize artworks into 20-year periods
-per <- seq(1540, 1750, by = 20)
-labelize <- function(x) {
-  label <- paste(x, "-", x + 19, sep="")
-  return(label)
-}
-per_labs <- vapply(per, labelize, FUN.VALUE = "character")
-location_data$period <- cut(location_data$dating.year, breaks = per)
-levels(location_data$period) <- per_labs
-
-place_stats <- location_data %>%
-  group_by(place) %>%
-  summarize(
-    count = n(),
-    longitude = min(longitude),
-    latitude = min(latitude),
-    min_start = min(dating.yearEarly),
-    max_start = max(dating.yearEarly),
-    min_end = (min(dating.yearLate)),
-    max_end = max(dating.yearLate),
-    date_sd = sd(dating.yearEarly),
-    date_var = var(dating.yearEarly),
-    date_mean = mean(dating.yearEarly)) %>%
-  mutate(spread = max_start - min_start)
-
-period_place_stats <- location_data %>%
-  group_by(place, period) %>%
-  summarize(
-    count = n(),
-    longitude = min(longitude),
-    latitude = min(latitude),
-    min_start = min(dating.yearEarly),
-    max_start = max(dating.yearEarly),
-    min_end = (min(dating.yearLate)),
-    max_end = max(dating.yearLate),
-    date_sd = sd(dating.yearEarly),
-    date_var = var(dating.yearEarly),
-    date_mean = mean(dating.yearEarly)) %>%
-  mutate(spread = max_start - min_start)
-
 location_data$short_place <- str_match(location_data$place, "(.*) \\(Amsterdam\\)")[,2]
-place_stats$short_place <- str_match(place_stats$place, "(.*) \\(Amsterdam\\)")[,2]
-period_place_stats$short_place <- str_match(period_place_stats$place, "(.*) \\(Amsterdam\\)")[,2]
 
-save(location_data, period_place_stats, file = "data/location_data.RData")
+save(location_data, file = "data/location_data.RData")
